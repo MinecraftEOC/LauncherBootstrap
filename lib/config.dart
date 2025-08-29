33ceae4b3d2d7a23905cf30bbcd0a1fc451cf431
Daniil Pinov <47653999+DaniilGit@users.noreply.github.com>
2025-08-29 12:32:46 +0700

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -2,5 +2,5 @@ class Config {
   static final Uri launcherJarUrl =
       Uri.parse('http://eoc-rp.ru/Launcher.jar');
 
-  static final String projectDirectoryName = 'eoc-test';
+  static final String projectDirectoryName = 'eoc-test-1';
 }
